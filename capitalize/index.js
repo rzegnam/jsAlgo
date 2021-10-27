7f16e73e72d6811de58b6b7bdb8d42b18d15362c
rzegnam <rzegnam@gmail.com>
2021-10-27 16:22:56 +0200

    capitalize my solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const capitalized = []
+    
+    for (let word of str.split(' ')) {
+        capitalized.push(word[0].toUpperCase() + word.slice(1))
+    }
+
+    return capitalized.join(' ')
+}
 
 module.exports = capitalize;
