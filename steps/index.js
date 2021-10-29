0a3bf0dd33c1003ed301b6934408ac851639adc5
rzegnam <rzegnam@gmail.com>
2021-10-29 15:21:54 +0200

    steps my solution

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,10 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log("#".repeat(i) + ' '.repeat(n - i))
+    }
+}
 
 module.exports = steps;
