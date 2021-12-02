f790927047950c932ccbd6535a208ca650520f79
rzegnam <rzegnam@gmail.com>
2021-12-02 14:10:29 +0100

    vowels - Stephens solution with match() and regex

--- vowels/index.js
+++ vowels/index.js
@@ -8,16 +8,8 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    let count = 0
-    const checker = ['a', 'e', 'i', 'o', 'u']
-
-    for (let char of str.toLowerCase()) {
-        if (checker.includes(char)) {
-            count++
-        }
-    }
-
-    return count
+    const matches = str.match(/[aeiou]/gi)
+    return matches ? matches.length : 0
 }
 
 module.exports = vowels;
@@ -33,6 +25,20 @@ module.exports = vowels;
 //     for (let i = 0; i < str.length; i++) {
 //         if (vowels.includes(str.toLowerCase()[i])) {
 //             count += 1
+//         }
+//     }
+
+//     return count
+// }
+
+// Stephens iterative solution
+// function vowels(str) {
+//     let count = 0
+//     const checker = ['a', 'e', 'i', 'o', 'u']
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count++
 //         }
 //     }
 
