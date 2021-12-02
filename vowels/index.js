dc8779ede196100d364bccf278a905c847a5e7bb
rzegnam <rzegnam@gmail.com>
2021-12-02 13:05:50 +0100

    vowels - my solution

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let count = 0
+
+    // iterate, if it's on the list +1
+    // how to iterate over the string, array, object - there is for of, for in, forEach ...
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str.toLowerCase()[i])) {
+            count += 1
+        }
+    }
+
+    return count
+}
 
 module.exports = vowels;
